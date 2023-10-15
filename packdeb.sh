ed6c943a883e24359fd2f75863765d9f4975a0a0
Nanashi <johnnyjel1234567890@gmail.com>
2023-10-15 12:48:25 -0500

    Update packdeb.sh

--- packdeb.sh
+++ packdeb.sh
@@ -4,8 +4,10 @@ rm -f genpwd-deb/usr/local/bin/genpwd.sh
 rm -f genpwd-deb/usr/local/bin/genpwd
 rm -f genpwd-deb/DEBIAN/control
 rm -f genpwd.deb
-cp genpwd.sh genpwd-deb/usr/local/bin/genpwd
-cp genpwd.sh genpwd-deb/usr/local/bin/genpwd.sh
+mkdir genpwd-deb/usr/local
+mkdir genpwd-deb/usr/local/bin
+cp ./genpwd.sh genpwd-deb/usr/local/bin/genpwd
+cp ./genpwd.sh genpwd-deb/usr/local/bin/genpwd.sh
 sed "s/VERSION/$EPOCH_TIME/g" control.bak > genpwd-deb/DEBIAN/control
 dpkg-deb -b genpwd-deb genpwd.deb
 rm -f genpwd-deb/usr/local/bin/genpwd.sh
