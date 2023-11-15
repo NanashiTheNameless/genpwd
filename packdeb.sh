ffa5117b6ae7db2d02affa27b5ffcd2b4638c893
CortezJEL <johnnyjel1234567890@gmail.com>
2023-11-15 17:41:57 -0600

    overhaul2

--- packdeb.sh
+++ packdeb.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 GENPWD_VERSION=$(date +%s)
 rm -rf ./tmp
-rm -rf Genpwd_$GENPWD_VERSION
-rm -f Genpwd_$GENPWD_VERSION.deb
-mkdir -p Genpwd_$GENPWD_VERSION/usr/local/bin
-cp ./genpwd.sh Genpwd_$GENPWD_VERSION/usr/local/bin/genpwd
-cp ./genpwd.sh Genpwd_$GENPWD_VERSION/usr/local/bin/genpwd.sh
-mkdir Genpwd_$GENPWD_VERSION/DEBIAN
-touch Genpwd_$GENPWD_VERSION/DEBIAN/control
+rm -rf genpwd
+rm -f genpwd.deb
+mkdir -p genpwd/usr/local/bin
+cp ./genpwd.sh genpwd/usr/local/bin/genpwd
+cp ./genpwd.sh genpwd/usr/local/bin/genpwd.sh
+mkdir genpwd/DEBIAN
+touch genpwd/DEBIAN/control
 echo "Package: genpwd
 Version: $GENPWD_VERSION
 Depends: bash, wget
@@ -17,15 +17,15 @@ Maintainer: Discord/NamelessNanashi
 Priority: optional
 Architecture: amd64
 Provides: genpwd
-Description: simple installer for my genpwd tool" > Genpwd_$GENPWD_VERSION/DEBIAN/control
-dpkg-deb --build --root-owner-group Genpwd_$GENPWD_VERSION
-rm -rf Genpwd_$GENPWD_VERSION
-dpkg -x Genpwd_$GENPWD_VERSION.deb ./tmp
-cd tmp ; tar -czvf Genpwd_$GENPWD_VERSION.debian.tar.gz * ; mv Genpwd_$GENPWD_VERSION.debian.tar.gz ../Genpwd_$GENPWD_VERSION.debian.tar.gz ; cd ..
+Description: simple installer for my genpwd tool" > genpwd/DEBIAN/control
+dpkg-deb --build --root-owner-group genpwd
+rm -rf genpwd
+dpkg -x genpwd.deb ./tmp
+cd tmp ; tar -czvf genpwd.orig.tar.gz * ; mv genpwd.orig.tar.gz ../genpwd.orig.tar.gz ; cd ..
 rm -rf ./tmp
 gh release create $GENPWD_VERSION --latest --generate-notes
 gh release upload $GENPWD_VERSION genpwd.sh
-gh release upload $GENPWD_VERSION Genpwd_$GENPWD_VERSION.deb
-gh release upload $GENPWD_VERSION Genpwd_$GENPWD_VERSION.debian.tar.gz
-rm -f Genpwd_$GENPWD_VERSION.deb
-rm -f Genpwd_$GENPWD_VERSION.tar.gz
+gh release upload $GENPWD_VERSION genpwd.deb
+gh release upload $GENPWD_VERSION genpwd.orig.tar.gz
+rm -f genpwd.deb
+rm -f genpwd.orig.tar.gz
