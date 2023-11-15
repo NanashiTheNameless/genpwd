3c9482c375958f7339a310997c32ed77d5b61312
CortezJEL <johnnyjel1234567890@gmail.com>
2023-11-15 17:35:34 -0600

    overhaul

--- packdeb.sh
+++ packdeb.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
-EPOCH_TIME=$(date +%s)
-rm -f genpwd-deb/usr/local/bin/genpwd.sh
-rm -f genpwd-deb/usr/local/bin/genpwd
-rm -f genpwd-deb/DEBIAN/control
-rm -f genpwd.deb
-mkdir genpwd-deb/usr/local
-mkdir genpwd-deb/usr/local/bin
-cp ./genpwd.sh genpwd-deb/usr/local/bin/genpwd
-cp ./genpwd.sh genpwd-deb/usr/local/bin/genpwd.sh
-sed "s/VERSION/$EPOCH_TIME/g" control.bak > genpwd-deb/DEBIAN/control
-dpkg-deb -b genpwd-deb genpwd.deb
-rm -f genpwd-deb/usr/local/bin/genpwd.sh
-rm -f genpwd-deb/usr/local/bin/genpwd
-rm -f genpwd-deb/DEBIAN/control
-gh release create $EPOCH_TIME --latest --generate-notes
-gh release upload $EPOCH_TIME genpwd.sh
-gh release upload $EPOCH_TIME genpwd.deb
-rm -f genpwd.deb
+GENPWD_VERSION=$(date +%s)
+rm -rf ./tmp
+rm -rf Genpwd_$GENPWD_VERSION
+rm -f Genpwd_$GENPWD_VERSION.deb
+mkdir -p Genpwd_$GENPWD_VERSION/usr/local/bin
+cp ./genpwd.sh Genpwd_$GENPWD_VERSION/usr/local/bin/genpwd
+cp ./genpwd.sh Genpwd_$GENPWD_VERSION/usr/local/bin/genpwd.sh
+mkdir Genpwd_$GENPWD_VERSION/DEBIAN
+touch Genpwd_$GENPWD_VERSION/DEBIAN/control
+echo "Package: genpwd
+Version: $GENPWD_VERSION
+Depends: bash, wget
+Recommends: zsh, tilix
+Section: genpwd
+Maintainer: Discord/NamelessNanashi
+Priority: optional
+Architecture: amd64
+Provides: genpwd
+Description: simple installer for my genpwd tool" > Genpwd_$GENPWD_VERSION/DEBIAN/control
+dpkg-deb --build --root-owner-group Genpwd_$GENPWD_VERSION
+rm -rf Genpwd_$GENPWD_VERSION
+dpkg -x Genpwd_$GENPWD_VERSION.deb ./tmp
+cd tmp ; tar -czvf Genpwd_$GENPWD_VERSION.debian.tar.gz * ; mv Genpwd_$GENPWD_VERSION.debian.tar.gz ../Genpwd_$GENPWD_VERSION.debian.tar.gz ; cd ..
+rm -rf ./tmp
+gh release create $GENPWD_VERSION --latest --generate-notes
+gh release upload $GENPWD_VERSION genpwd.sh
+gh release upload $GENPWD_VERSION Genpwd_$GENPWD_VERSION.deb
+gh release upload $GENPWD_VERSION Genpwd_$GENPWD_VERSION.debian.tar.gz
+rm -f Genpwd_$GENPWD_VERSION.deb
+rm -f Genpwd_$GENPWD_VERSION.tar.gz
